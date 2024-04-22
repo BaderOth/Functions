05697b0d30237f3f334a680a1ca7525b907ee29f
BaderOth <bader0198@gmail.com>
2024-04-22 15:52:10 +0300

    Mult function

--- Test/src/App.java
+++ Test/src/App.java
@@ -2,4 +2,20 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    public static int mult(int x, int y) {
+        int result = 1;
+        if (x < y) {
+            for (int i = x; i <= y; i++) {
+                result *= i;
+            }
+        } else if (x > y) {
+            for (int i = y; i <= x; i++) {
+                result *= i;
+            }
+        } else {
+            return x;
+        }
+        return result;
+    }
 }
