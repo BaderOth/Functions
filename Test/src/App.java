658a9b69a3c5868628a7458c8125f8f43b665ece
BaderOth <bader0198@gmail.com>
2024-04-22 16:14:38 +0300

    Push all

--- Test/src/App.java
+++ Test/src/App.java
@@ -1,6 +1,13 @@
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        int y = in.nextInt();
+        System.out.println(mod(x, y));
+        System.out.println(mult(x, y));
+        System.out.println(add(x, y));
     }
 
     public static int mod(int x, int y) {
