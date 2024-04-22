b3b651fa74e9147b3d422b8008c2127b03f6028c
jibirlkabbara <jebrilbilokabbara@gmail.com>
2024-04-22 15:50:47 +0300

    modulus function

--- Test/src/App.java
+++ Test/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        System.out.println(calculateModulus(4,2));
+    }
+    public static int calculateModulus(int x, int y) {
+        return x % y;
     }
 }
