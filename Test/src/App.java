94ccea50118466507e47487ea15764afab93aefa
AbdulrahmanHalabi <halabiabdulrahman5@gmail.com>
2024-04-22 16:10:49 +0300

    mod function

--- Test/src/App.java
+++ Test/src/App.java
@@ -1,10 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        
+
     }
     
     public static int mod(int x, int y){
-        return x%y;
+        return  x%y;
+
+
     }
     }
